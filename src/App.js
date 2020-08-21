926bbc72f75c017233cfdfceb9532b4153e31f2c
purplehays09 <purplehays527@gmail.com>
2020-08-21 09:39:10 -0600

    I finished step 1 for the ui on the home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import './App.css';
+import * as yup from 'yup'
+// import formSchema from './validation/formSchema'
+// import Form from './form'
+
+
+//Step 1 Build the UI to have the basic options that you need according to the 
+//Step 2 Set up the states
+//Step 3 set up the functions and useEffects to change the states
+//Step 4 pass them to the form.js component via props
+//Step 5 retrieve the propos on the form.js component and build out the ui with all the form fun stuff
+//Step 6 apply the props to the form
+//Step 7 Build out the routes
+//Step 8 Build out the validation with yup
+//Step 9 Build out the testing with cypress
+//STRETCH
+
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <header>
+
+        <nav>
+          <h3>
+            LAMBDA EATS
+          </h3>
+          <div>
+            <a href='/'>Home</a>
+            <a href='/'>Help</a>
+          </div>
+        </nav>
+
+        <div>
+          <h1>Lambda Eats</h1>
+          <p>Your favorite food delivered while coding</p>
+          <button id='pizzaBtn'>PIZZA</button>
+
+        </div>
+      </header>
+
+
+      
+    </div>
   );
 };
 export default App;
